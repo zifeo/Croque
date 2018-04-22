1a95b0a802dc3c6a703453d4b551af0f8fb1a751
Teo Stocco <teo@zifeo.com>
2018-04-22 23:24:53 +0200

    fix build

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ import Cron from 'cron';
 import { computeNextNoon } from './helpers';
 import lowdbFactory from './db';
 import config from './config';
-import { lunchCron, reminderCron } from './crons';
+import { lunchCron /* reminderCron */ } from './crons';
 
 if (config.production) {
   Raven.config(config.raven, {
@@ -53,8 +53,8 @@ lowdbFactory().then(db => {
   const lunchBeat = new Cron.CronJob('00 00 12 * * 2,4', lunchCron(db, transporter), null, false, config.tz);
   lunchBeat.start();
 
-  //const reminderBeat = new Cron.CronJob('00 00 10 * * 2,4', reminderCron(db, transporter), null, false, config.tz);
-  //reminderBeat.start();
+  // const reminderBeat = new Cron.CronJob('00 00 10 * * 2,4', reminderCron(db, transporter), null, false, config.tz);
+  // reminderBeat.start();
 
   const tequilaStrategy = new tequilaPassport.Strategy(config.tequila);
   passport.use(tequilaStrategy);
