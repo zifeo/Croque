883273970c317ab391138daa2c9974cd8da25efc
Teo Stocco <teo@zifeo.com>
2018-10-13 14:43:43 +0200

    wip locations

--- src/locations.js
+++ src/locations.js
@@ -7,6 +7,43 @@ type Location = {
   plan: string,
 };
 
+/*
+
+Unimail Tram
+Sous le dernier arbre en face de la sortie à droite en sortant d'unimail
+https://maps.google.com/?q=46.194625,6.140431
+
+Unimail Parc
+En sortant d'unimail sur la droite vers la boulangerie poully
+https://maps.google.com/?q=46.195478,6.139574
+
+Plainpalais South
+À l'angle de l'avenue Henri dunant et de l'avenue du mail
+???
+
+Plainpalais West
+Sur le trottoir de plainpalais en face de l'éléphant de la cannette (traverser la route depuis l'éléphant et attendre sur le trottoir opposé)
+https://maps.google.com/?q=46.197798,6.139269
+
+Plainpalais East
+En haut des escaliers du parking de plainpalais, pas loin de la buvette.
+https://maps.google.com/?q=46.198235,6.141788
+
+Plainpalais North
+Derrière le Skatepark côté route Henri Marc
+https://maps.google.com/?q=46.199703,6.140262
+
+Dufour Entrance
+vers le panneau parking du parking de vélos pile en face de la sortie
+https://maps.google.com/?q=46.199780,6.142794
+
+Grand Théâtre
+Sur les escaliers du grand théâtre, à gauche en étant face au théâtre.
+https://maps.google.com/?q=46.201440,6.142915
+---> plutôt uni bastion ?
+
+ */
+
 const locations: Array<Location> = [
   {
     id: 'uni-1',
